b39dc34d7a6018f9862422df2c2aca580cebd697
Joe Schaffer <schaffernation@gmail.com>
2014-01-19 13:11:39 -0500

    Initial Commit

---
+++ app/WDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDAppDelegate.h
+//  app
+//
+//  Created by Joseph Schaffer on 1/19/14.
+//  Copyright (c) 2014 Who's Down. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  app
+//
+//  Created by Joseph Schaffer on 1/19/14.
+//  Copyright (c) 2014 Who's Down. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WDAppDelegate class]));
+    }
+}
